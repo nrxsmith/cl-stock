7da4082f19c39108443c7774410a300ac46617de
nrxsmith <nathanrobert.smith@gmail.com>
2017-07-22 18:52:19 -0400

    Make displayer read from config file

--- bin/stock.rb
+++ bin/stock.rb
@@ -1,8 +1,18 @@
 require_relative '../lib/displayer.rb'
 
+CONFIG_PATH = '/home/nate/code/personal/stock/config/tickers.txt'
+
 s = StockDisplayer.new
-s.perform("Dow")
-s.perform("S&P 500")
-s.perform("Nasdaq")
-s.perform("BND")
-s.perform("VXUS")
+
+File.foreach(CONFIG_PATH) {|t| s.perform(t.chomp) }
+
+# tickers = ["Dow",
+#           "S&P 500",
+#           "Nasdaq",
+#           "BND",
+#           "VXUS"]
+
+# tickers.each do |t|
+#  s.perform(t)
+# end
+
